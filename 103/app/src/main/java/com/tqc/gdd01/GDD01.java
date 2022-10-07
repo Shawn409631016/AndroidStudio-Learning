acf50dea6062f89b4ceb19ebe34445e8dfb1c426
shawn <shawnfeng610@gmail.com>
2022-10-07 10:18:36 +0800

    add 103

---
+++ 103/app/src/main/java/com/tqc/gdd01/GDD01.java
@@ -0,0 +1,21 @@
+package com.tqc.gdd01;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+public class GDD01 extends Activity
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+   // TO DO
+
+
+  }
+}
